b36cc469bc52cd704620c0e0a9611cc5c79704c5
Andre Lassalvia <andrelassalvia@gmail.com>
2021-09-22 14:46:12 +0100

    @isset, @empty, ternary, ??default

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,7 +8,32 @@ class FornecedorController extends Controller
 {
     public function index(){
         $fornecedores = [
-            0 => ['nome'=> 'Fornecedor1', 'status'=>'N']
+            0 => [
+                'nome'=> 'Fornecedor1', 
+                'status'=>'N',
+                'cnpj' => '00.000.000/000-00'
+            ],
+            
+            1 => [
+                'nome' => 'Fornecedor2',
+                'status' => 'S',
+                'cnpj' => '01.000.000/0001-00'
+            ],
+
+            2 => [
+                'nome' => 'Fornecedor3',
+                'status' => 'S',
+                'cnpj' => ''
+                
+            ],
+
+            3 => [
+                'nome' => 'Fornecedor4',
+                'status' => 'N',
+                
+                
+            ]
+
         ];
         
         return view('app.fornecedor.index', compact('fornecedores'));
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -32,15 +32,102 @@
 
 {{-- @dd($fornecedores) --}}
 
-Fornecedor:{{$fornecedores[0]['nome']}}
-<br>
-Status:{{$fornecedores[0]['status']}}
-@if($fornecedores[0]['status'] === 'N')
+{{-- @if($fornecedores[0]['status'] === 'N')
     <p>Fornecedor inativo</p>
 
-@endif
+@endif --}}
 {{-- mas podemos usar o @unless para fazer a negacao como se fosse ! --}}
 
-@unless($fornecedores[0]['status'] === 'S')
+{{-- @unless($fornecedores[0]['status'] === 'S')
 Fornecedor Inativo
-@endunless
+@endunless --}}
+
+{{-- @isset - verificar se variaveis, arrays estao definidos. Retorna True or False --}}
+
+
+@isset($fornecedores)
+    CNPJ: {{ $fornecedores[3]['cnpj'] ?? 'Dado nao foi preenchido' }}
+    <br>
+    <br>
+    @isset($fornecedores[0]['nome'])
+    Fornecedor: {{$fornecedores[0]['nome']}}
+    @endisset
+    <br>
+    @isset($fornecedores[0]['status'])
+    Status:{{$fornecedores[0]['status']}}
+    @endisset
+    <br>
+    @isset($fornecedores[0]['cnpj'])
+    CNPJ:{{$fornecedores[0]['cnpj']}}
+        @empty($fornecedores[0]['cnpj'])
+        Nao definidos
+        @endempty
+    @endisset
+    <br>
+    <br>
+    @isset($fornecedores[1]['nome'])
+    Fornecedor: {{$fornecedores[1]['nome']}}
+    @endisset
+    <br>
+    @isset($fornecedores[1]['status'])
+    Status:{{$fornecedores[1]['status']}}
+    @endisset
+    <br>
+    @isset($fornecedores[1]['cnpj'])
+    CNPJ:{{$fornecedores[1]['cnpj']}}
+        @empty($fornecedores[1]['cnpj'])
+        Nao definidos
+        @endempty
+    @endisset
+    <br>
+    <br>
+    @isset($fornecedores[2]['nome'])
+    Fornecedor: {{$fornecedores[2]['nome']}}
+    @endisset
+    <br>
+    @isset($fornecedores[2]['status'])
+    Status:{{$fornecedores[2]['status']}}
+    @endisset
+    <br>
+    @isset($fornecedores[2]['cnpj'])
+    CNPJ:{{$fornecedores[2]['cnpj']}} 
+        @empty($fornecedores[2]['cnpj'])
+        Nao definidos
+        @endempty
+        <br>
+      
+    @endisset
+    <br>
+    <br>
+    @isset($fornecedores[3]['nome'])
+    Fornecedor: {{$fornecedores[3]['nome']}}
+    @endisset
+    <br>
+    @isset($fornecedores[3]['status'])
+    Status:{{$fornecedores[3]['status']}}
+    @endisset
+    <br>
+    @isset($fornecedores[3]['cnpj'])
+    CNPJ:{{$fornecedores[3]['cnpj'] ?? 'cnpj nao foi preenchido'}} {{-- nao funciona pois o @isset ja esta trabalhando --}}
+        @empty($fornecedores[3]['cnpj'])
+        Nao definidos
+        @endempty
+        <br>
+    @endisset
+    <br>
+    <br>
+@endisset
+
+{{-- @empty - VERIFICA SE VARIAVEIS E ARRAYS ESTAO VAZIOS --}}
+{{-- Consideramos variavel ou array vazia quando apresenta os seguintes valores:
+- '' -> string vazia
+- 0
+- 0.0
+- '0'
+- null
+- false
+- array() -> array vazio
+- $var -> variavel declarada mas nao alocado nenhum valor --
+
+NESTAS SITUACAO O empty RETORNA TRUE --}}
+
