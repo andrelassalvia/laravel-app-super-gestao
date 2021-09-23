663419acbdbe4785edc32d16ec3177a71d595edd
Andre Lassalvia <andrelassalvia@gmail.com>
2021-09-23 10:51:42 +0100

    Switch, For, While

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -9,28 +9,35 @@ class FornecedorController extends Controller
     public function index(){
         $fornecedores = [
             0 => [
-                'nome'=> 'Fornecedor1', 
+                'nome'=> 'Fornecedor 1', 
                 'status'=>'N',
-                'cnpj' => '00.000.000/000-00'
+                'cnpj' => '00.000.000/000-00',
+                'DDD' => '11', // Sao Paulo (SP)
+                'telefone' => '2547-5587'
             ],
             
             1 => [
-                'nome' => 'Fornecedor2',
+                'nome' => 'Fornecedor 2',
                 'status' => 'S',
-                'cnpj' => '01.000.000/0001-00'
+                'cnpj' => '01.000.000/0001-00',
+                'DDD' => '85', // Fortaleza (CE)
+                'telefone' => '0001-2541'
             ],
 
             2 => [
-                'nome' => 'Fornecedor3',
+                'nome' => 'Fornecedor 3',
                 'status' => 'S',
-                'cnpj' => ''
+                'cnpj' => '',
+                'DDD' => '32', // Juiz de Fora (MG)
+                'telefone' => '4474-5587'
                 
             ],
 
             3 => [
-                'nome' => 'Fornecedor4',
+                'nome' => 'Fornecedor 4',
                 'status' => 'N',
-                
+                'DDD' => '',
+                               
                 
             ]
 
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,6 +2,9 @@
 
 {{-- aqui dentro fica o comentario que nao sera interpretado. --}}
 
+{{-- ========================================================= --}}
+
+{{-- Secao pura de PHP dentro do Blade --}}
 @php
  // para fazermos uma secao pura de php basta iniciarmos e fecharmos uma secao com @
 
@@ -16,8 +19,13 @@
 
 @endphp
 
+{{-- =============================================================== --}}
+
 {{-- para usar um array vindo de uma variavel no laravel  --}}
 {{-- @dd($fornecedores) --}}
+
+{{-- ============================================================== --}}
+{{-- IF ELSE NO LARAVEL --}}
 
 {{-- @if(count($fornecedores) > 0 && count($fornecedores) < 10){
     <h3>Existem alguns fornecedores cadastrados</h3>
@@ -27,6 +35,9 @@
     <h3>Nao existem fornecedores cadastrados.</h3>
 }
 @endif --}}
+
+{{-- ================================================================= --}}
+{{-- UNLESS -> O INVERSO DO IF --}}
 
 {{-- @unless é uma inversao de @if --}}
 
@@ -42,10 +53,27 @@
 Fornecedor Inativo
 @endunless --}}
 
+
+{{-- ======================================================================= --}}
+{{-- ISSET -> EXISTE A VARIAVEL OU ARRAY? --}}
 {{-- @isset - verificar se variaveis, arrays estao definidos. Retorna True or False --}}
 
 
-@isset($fornecedores)
+
+{{-- EMPTY -> A VARIAVEL EXISTE, MAS ELA POSSUI ALGUM VALOR? --}}
+{{-- Consideramos variavel ou array vazia quando apresenta os seguintes valores:
+- '' -> string vazia
+- 0
+- 0.0
+- '0'
+- null
+- false
+- array() -> array vazio
+- $var -> variavel declarada mas nao alocado nenhum valor --
+
+NESTAS SITUACAO O empty RETORNA TRUE --}}
+
+{{-- @isset($fornecedores)
     CNPJ: {{ $fornecedores[3]['cnpj'] ?? 'Dado nao foi preenchido' }}
     <br>
     <br>
@@ -107,27 +135,128 @@ Fornecedor Inativo
     Status:{{$fornecedores[3]['status']}}
     @endisset
     <br>
-    @isset($fornecedores[3]['cnpj'])
-    CNPJ:{{$fornecedores[3]['cnpj'] ?? 'cnpj nao foi preenchido'}} {{-- nao funciona pois o @isset ja esta trabalhando --}}
-        @empty($fornecedores[3]['cnpj'])
+    @isset($fornecedores[3]['cnpj']) --}}
+    {{-- CNPJ:{{$fornecedores[3]['cnpj'] ?? 'cnpj nao foi preenchido'}} nao funciona pois o @isset ja esta trabalhando --}}
+        {{-- @empty($fornecedores[3]['cnpj'])
         Nao definidos
         @endempty
         <br>
     @endisset
     <br>
     <br>
+@endisset --}}
+
+
+
+
+{{-- ======================================================= --}}
+{{-- OPERADOR TERNARIO ?? --}}
+
+{{-- SWITCH CASE NO BLADE --}}
+{{-- @isset($fornecedores)
+    Fornecedor: {{$fornecedores[3]['nome']}}
+    <br>
+    Status: {{$fornecedores[3]['status']}}
+    <br>
+    CNPJ: {{$fornecedores[3]['cnpj'] ?? 'cnpj nao informado'}}
+    <br>
+    DDD: {{$fornecedores[3]['DDD'] ?? 'DDD nao informado'}}
+    Telefone: {{$fornecedores[3]['telefone'] ?? 'Telefone nao informado'}}
+
+    @switch($fornecedores[3]['DDD'])
+        @case('11')
+            Sao Paulo - SP
+        @break
+        @case('85')
+            Fortaleza - CE
+        @break
+        @case('32')
+            Juiz de Fora - MG
+        @break
+        @default
+         
+        @break
+
+    @endswitch
+
+@endisset --}}
+
+
+{{-- ======================================================== --}}
+{{-- FOR NO BLADE --}}
+
+{{-- @isset($fornecedores)
+
+    
+    @for ($i = 0;  isset($fornecedores[$i]) ; $i++)
+
+        Fornecedor: {{$fornecedores[$i]['nome'] ?? 'Não existe este fornecedor'}}
+        <br>
+        Status: {{$fornecedores[$i]['status'] ?? 'Status não identificado'}}
+        <br>
+        CNPJ: {{$fornecedores[$i]['cnpj'] ?? 'não informado'}}
+        <br>
+        DDD: {{$fornecedores[$i]['DDD'] ?? 'DDD não informado'}}
+        Telefone: {{$fornecedores[$i]['telefone'] ?? 'não informado'}}
+
+        @switch($fornecedores[$i]['DDD'])
+
+            @case('11')
+                São Paulo - SP
+            @break
+            @case('85')
+                Fortaleza - CE
+            @break
+            @case('32')
+                Juiz de Fora - MG
+            @break
+            @default
+            @break
+
+        @endswitch
+        <br>
+        <hr>
+        <br>
+
+    @endfor
+
+@endisset --}}
+
+{{-- ============================================================ --}}
+{{-- WHILE NO BLADE --}}
+
+@isset($fornecedores)
+
+    @php $i = 0 @endphp
+    @while(isset($fornecedores[$i]))
+        Fornecedor: {{$fornecedores[$i]['nome'] ?? 'Não existe este fornecedor'}}
+        <br>
+        Status: {{$fornecedores[$i]['status'] ?? 'Status não identificado'}}
+        <br>
+        CNPJ: {{$fornecedores[$i]['cnpj'] ?? 'não informado'}}
+        <br>
+        DDD: {{$fornecedores[$i]['DDD'] ?? 'DDD não informado'}}
+        Telefone: {{$fornecedores[$i]['telefone'] ?? 'não informado'}}
+
+        @switch($fornecedores[$i]['DDD'])
+
+            @case('11')
+                São Paulo - SP
+                @break
+            @case('85')
+                Fortaleza - CE
+                @break
+            @case('32')
+                Juiz de Fora - MG
+                @break
+            @default
+                @break
+
+        @endswitch
+        @php $i++ @endphp
+        <br>
+        <hr>
+        <br>
+    @endwhile
 @endisset
-
-{{-- @empty - VERIFICA SE VARIAVEIS E ARRAYS ESTAO VAZIOS --}}
-{{-- Consideramos variavel ou array vazia quando apresenta os seguintes valores:
-- '' -> string vazia
-- 0
-- 0.0
-- '0'
-- null
-- false
-- array() -> array vazio
-- $var -> variavel declarada mas nao alocado nenhum valor --
-
-NESTAS SITUACAO O empty RETORNA TRUE --}}
 
